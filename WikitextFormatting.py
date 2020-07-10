3692049c4d8988e46b7ca568b97c82981c686e54
Lucas Kersken <kersken.l@live.de>
2020-07-10 13:26:06 +0200

    Initial Commit with planned classes

---
+++ WikitextFormatting.py
@@ -0,0 +1,11 @@
+
+class WikitextChainFormatter:
+    """
+    Format Wikipedia Texts and extract chain via different criteria from them
+    """
+
+
+class WikitextCriteria:
+    """
+    Use to define what kind of words should be chosen from a given text
+    """
