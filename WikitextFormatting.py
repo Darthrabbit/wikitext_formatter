a59ffe02161718d5723bdf7c2aa8538550a6599c
Lucas Kersken <kersken.l@live.de>
2020-07-10 16:44:46 +0200

    Split Wikitext class into static and classmethods. Introduced factory concept
    Extracted all static methods from the class
    changed constructor
    	Now can construct frome file or dataframe
    	if classmethods are called the data will be filtered
    	if construcor is used nothing will be changed

--- WikitextFormatting.py
+++ WikitextFormatting.py
@@ -4,89 +4,114 @@ import pandas as pd
 from abc import ABC, abstractmethod
 
 
+def read_file(path):
+    with open(path, "r") as fp:
+        content = fp.readlines()
+
+    for i in range(len(content)):
+        content[i] = content[i].strip()
+        content[i] += '\n'
+
+    content = "".join(content)
+    return content
+
+
+def split_into_titles_and_articles(parsed):
+    def get_header_level(header):
+        return int(header.count('=') / 2 + 1)
+
+    labels = []
+    articles = []
+    new_article = []
+    is_new = False
+
+    labels.append(parsed.sections[1].title)
+    new_article.append(parsed.sections[1].contents)
+
+    for sec in parsed.sections[2:]:
+        if get_header_level(sec.title) == 1:
+            labels.append(sec.title)
+            articles.append("".join(new_article))
+            new_article = []
+
+        new_article.append(sec.contents)
+
+    articles.append("".join(new_article))
+
+    return labels, articles
+
+
+def remove_punctuation(article, exceptions):
+    words = article.split(" ")
+    words = [word for word in words if (word.isalpha() or word in exceptions)]
+    cleaned = " ".join(words)
+
+    return cleaned
+
+
+def stop_word_removal(article, stop_words):
+    words = article.split(" ")
+    words = [word for word in words if word not in stop_words]
+    cleaned = " ".join(words)
+
+    return cleaned
+
+
+def clean_article(article, exceptions, stop_words, min_length):
+    sentences = nltk.sent_tokenize(article)
+    sentences = [sent.lower() for sent in sentences]
+    sentences = [sent.strip() for sent in sentences]
+    sentences = [sent for sent in sentences if len(sent) >= min_length]
+    sentences = [stop_word_removal(sent, stop_words) for sent in sentences]
+    sentences = [remove_punctuation(sent, exceptions) for sent in sentences]
+    sentences = [sent + "\n" for sent in sentences if len(sent) > min_length]
+
+    return "".join(sentences)
+
+
+def clean_titles(title, exceptions, stop_words):
+    cleaned_title = title.lower().strip()
+    cleaned_title = stop_word_removal(cleaned_title, stop_words)
+    cleaned_title = remove_punctuation(cleaned_title, exceptions)
+
+    return "".join(cleaned_title)
+
+
 class WikitextChainFormatter:
     """
     Format Wikipedia Texts and extract chain via different criteria from them
     """
 
-    def __init__(self, pathToFile, stopWords, exceptions, minArticleLength):
-
-        parsed = wtp.parse(self.read_file(pathToFile))
-        labels, articles = self.split_into_titles_and_articles(parsed)
-
-        labels = [self.clean_titles(label, exceptions, stopWords) for label in labels]
-        articles = [self.clean_article(article, exceptions, stopWords, minArticleLength) for article in
-                    articles]
-
-        self.data = pd.DataFrame({"Labels": labels, "Articles": articles})
+    def __init__(self, data):
+        self.data = data
         self.frequency = self.get_frequency_from_data()
 
-    def read_file(self, path):
-        with open(path, "r") as fp:
-            content = fp.readlines()
+    @classmethod
+    def from_file(cls, pathToFile, minArticleLength, stopWords=None, exceptions=None):
+        if exceptions is None:
+            exceptions = []
+        if stopWords is None:
+            stopWords = []
 
-        for i in range(len(content)):
-            content[i] = content[i].strip()
-            content[i] += '\n'
+        parsed = wtp.parse(read_file(pathToFile))
+        labels, articles = split_into_titles_and_articles(parsed)
 
-        content = "".join(content)
-        return content
+        labels = [clean_titles(label, exceptions, stopWords) for label in labels]
+        articles = [clean_article(article, exceptions, stopWords, minArticleLength) for article in articles]
 
-    def split_into_titles_and_articles(self, parsed):
-        def get_header_level(header):
-            return int(header.count('=') / 2 + 1)
+        return cls(pd.DataFrame({"Labels": labels, "Articles": articles}))
 
-        labels = []
-        articles = []
-        new_article = []
-        is_new = False
+    @classmethod
+    def from_dataframe(cls, df, minArticleLength, stopWords=None, exceptions=None):
+        if exceptions is None:
+            exceptions = []
+        if stopWords is None:
+            stopWords = []
 
-        labels.append(parsed.sections[1].title)
-        new_article.append(parsed.sections[1].contents)
+        df["Lables"] = [clean_titles(label, exceptions, stopWords) for label in df["Lables"]]
+        df["Articles"] = [clean_article(article, exceptions, stopWords, minArticleLength) for article in df["Articles"]]
 
-        for sec in parsed.sections[2:]:
-            if get_header_level(sec.title) == 1:
-                labels.append(sec.title)
-                articles.append("".join(new_article))
-                new_article = []
-
-            new_article.append(sec.contents)
-
-        articles.append("".join(new_article))
-
-        return labels, articles
-
-    def remove_punctuation(self, article, exceptions):
-        words = article.split(" ")
-        words = [word for word in words if (word.isalpha() or word in exceptions)]
-        cleaned = " ".join(words)
-
-        return cleaned
-
-    def stop_word_removal(self, article, stop_words):
-        words = article.split(" ")
-        words = [word for word in words if word not in stop_words]
-        cleaned = " ".join(words)
-
-        return cleaned
-
-    def clean_article(self, article, exceptions, stop_words, min_length):
-        sentences = nltk.sent_tokenize(article)
-        sentences = [sent.lower() for sent in sentences]
-        sentences = [sent.strip() for sent in sentences]
-        sentences = [sent for sent in sentences if len(sent) >= min_length]
-        sentences = [self.stop_word_removal(sent, stop_words) for sent in sentences]
-        sentences = [self.remove_punctuation(sent, exceptions) for sent in sentences]
-        sentences = [sent + "\n" for sent in sentences if len(sent) > min_length]
-
-        return "".join(sentences)
-
-    def clean_titles(self, title, exceptions, stop_words):
-        cleaned_title = title.lower().strip()
-        cleaned_title = self.stop_word_removal(cleaned_title, stop_words)
-        cleaned_title = self.remove_punctuation(cleaned_title, exceptions)
-
-        return "".join(cleaned_title)
+        return cls(df)
 
     def get_frequency_from_data(self):
         return nltk.FreqDist(nltk.word_tokenize(" ".join([" ".join(self.data[frame]) for frame in self.data])))
