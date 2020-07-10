516627bb7ce20609d5000197d52fd85beba2c89c
Lucas Kersken <kersken.l@live.de>
2020-07-10 14:10:48 +0200

    Read and parse file

--- WikitextFormatting.py
+++ WikitextFormatting.py
@@ -1,8 +1,28 @@
+import wikitextparser as wtp
+import nltk
+import pandas as pd
+
 
 class WikitextChainFormatter:
     """
     Format Wikipedia Texts and extract chain via different criteria from them
     """
+
+    def __init__(self, pathToFile, criteria, stopWords, exceptions, minArticleLength):
+        self.pathToFile = pathToFile
+
+        parsed = wtp.parse(self.read_file(pathToFile))
+
+    def read_file(self, path):
+        with open(path, "r") as fp:
+            content = fp.readlines()
+
+        for i in range(len(content)):
+            content[i] = content[i].strip()
+            content[i] += '\n'
+
+        content = "".join(content)
+        return content
 
 
 class WikitextCriteria:
