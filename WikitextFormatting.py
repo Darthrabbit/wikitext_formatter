ef7aac1e1077825b60c544deccb9da349f661036
Lucas Kersken <kersken.l@live.de>
2020-07-10 14:11:42 +0200

    Splitting into titles and articles
    Now the wikitext file can be split into lables and articels/data
    Using Wikitextparser https://github.com/5j9/wikitextparser

--- WikitextFormatting.py
+++ WikitextFormatting.py
@@ -12,6 +12,7 @@ class WikitextChainFormatter:
         self.pathToFile = pathToFile
 
         parsed = wtp.parse(self.read_file(pathToFile))
+        labels, articles = self.split_into_titles_and_articles(parsed)
 
     def read_file(self, path):
         with open(path, "r") as fp:
@@ -23,6 +24,30 @@ class WikitextChainFormatter:
 
         content = "".join(content)
         return content
+
+    def split_into_titles_and_articles(self, parsed):
+        def get_header_level(header):
+            return int(header.count('=') / 2 + 1)
+
+        labels = []
+        articels = []
+        new_articel = []
+        is_new = False
+
+        labels.append(parsed.sections[1].title)
+        new_articel.append(parsed.sections[1].contents)
+
+        for sec in parsed.sections[2:]:
+            if get_header_level(sec.title) == 1:
+                labels.append(sec.title)
+                articels.append("".join(new_articel))
+                new_articel = []
+
+            new_articel.append(sec.contents)
+
+        articels.append("".join(new_articel))
+
+        return labels, articels
 
 
 class WikitextCriteria:
